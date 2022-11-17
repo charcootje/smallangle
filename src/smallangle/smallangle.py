d8791f2710141842f19f8effa64536a802063fa6
charcootje <chachablok@gmail.com>
2022-11-17 12:41:04 +0100

    Command creeeren

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -4,19 +4,38 @@ from numpy import pi
 import pandas as pd
 
 
+@click.group()
+def cmd_group():
+    """Adds functions to one group
+    """    
+    pass
+
+@cmd_group.command()
+@click.option("-n", "--number", default=0)
 def sin(number):
-    x = np.linspace(0, 2 * pi, number)
+    """Calculate the sinus of a number of steps
+
+    Args:
+        number (integer): number of steps
+    """    
+    x = np.linspace(0, 2 * pi, int(number))
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
-
+@cmd_group.command()
+@click.option("-n", "--number", default=pi)
 def tan(number):
-    x = np.linspace(0, 2 * pi, number)
+    """Calculate the tangens of a number of steps
+
+    Args:
+        number (integer): number of steps
+    """    
+    x = np.linspace(0, 2 * pi, int(number))
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
     return
 
 
 if __name__ == "__main__":
-    sin(10)
+    cmd_group()
